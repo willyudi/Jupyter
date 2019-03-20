69223760b677f63996533ca8a3cefddfc03a0828
Williano <willyudi@gmail.com>
2019-03-20 00:10:06 -0300

    initial commit

---
+++ ml.py
@@ -0,0 +1,26 @@
+import pandas as pd
+df = pd.read_csv('iris.data', header=None)
+df.tail()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+# plt.savefig('images/02_06.png', dpi=300)
+plt.show()
